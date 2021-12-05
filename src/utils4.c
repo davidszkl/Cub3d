4dee0ebacfa82b645d20c4128fdad44fc525f23f
David Szklarzewski <dszklarz@fu-r2-p4.s19.be>
2021-12-05 14:34:46 +0100

    cleanup

--- src/utils4.c
+++ src/utils4.c
@@ -9,6 +9,7 @@
 /*   Updated: 2021/12/04 15:16:17 by dszklarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "utils.h"
 
 int	ft_tablen(char **tab)
 {
@@ -18,4 +19,27 @@ int	ft_tablen(char **tab)
 	while (tab[n])
 		n++;
 	return (n);
+}
+
+int	ft_atoi_mod(char *str, int *modify)
+{
+	int	n;
+	int	j;
+
+	n = 0;
+	j = 0;
+	while (str[j] == ' ' || str[j] == ',' || str[j] == 9)
+		j++;
+	if (!ft_isdigit(str[j]))
+		return (-1);
+	while (ft_isdigit(str[j]))
+	{
+		if (n < 0 || n > 255)
+			return (-1);
+		n = n * 10 + (str[j++] - '0');
+	}
+	if (n < 0 || n > 255)
+		return (-1);
+	*modify = n;
+	return (j);
 }
